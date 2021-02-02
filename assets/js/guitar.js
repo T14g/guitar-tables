17ebe08ac0f2fa9b2dc2ae8ea020529e4b3c6db5
Tiago <ottiagos@gmail.com>
2021-02-01 23:34:35 -0300

    Cronômetro base

--- assets/js/guitar.js
+++ assets/js/guitar.js
@@ -856,3 +856,82 @@ loadHome();
 // patchTable('12345');
 
 
+//Cronometro
+// Rebound of this pen https://codepen.io/Tonetete/pen/fzDop
+$(function(){
+
+    var t = undefined;
+    var log = $("#log");
+    var cl_thousandths = $("#thousandths");
+    var cl_seconds = $("#seconds");
+    var cl_minutes = $("#minutes");
+	
+    function showTimer() {
+      if(Number(cl_thousandths.html())%999>0.8){
+        cl_thousandths.html("0.00");
+        printDigit(cl_seconds);
+        if(Number(cl_seconds.html())>59){
+          cl_seconds.html("00");
+          printDigit(cl_minutes);
+
+        }
+       
+		    }
+   else           cl_thousandths.html((+cl_thousandths.html() + 0.015).toFixed(2));
+		
+	  }
+	
+	  function initTimer() {
+		    t=setInterval(showTimer, 20);
+	  }
+
+	  function stopTimer() {
+      clearInterval(t);
+      t=undefined;
+      if($.trim($("#log").html())==''){
+        log.html("<thead><tr><th>Stop Times</th></tr></thead>");
+        log.show();
+      }
+      		log.html(log.html() + "<tr><td>"+cl_minutes.html() +":"+cl_seconds.html()+":"+ cl_thousandths.html()+"</td><td></tr>");
+	  }
+	
+   function changeTimer() {
+      (!t)?initTimer() : stopTimer();
+   }
+	// Reset chronometer and clean up log.
+   function resetTimer() {
+      if (!t) {
+        cl_thousandths.html("0.00");
+        cl_seconds.html("00");
+        cl_minutes.html("00");
+        log.html("");
+        log.hide();
+      }
+	   }
+
+	
+	   function printDigit(digit){
+     
+      var number = Number(digit.html().replace(/\s+/g, "")) + 1;
+      var numberString;
+     
+		      (number<10)?numberString="0"+number : numberString=String(number);
+     
+      if(numberString.substring(1,2)==="1"){
+        if(number<10)
+          digit.html("0 "+number);
+        else
+          digit.html(numberString.split("").join(" "));
+      }
+     
+      else{
+        if(number<10)
+          digit.html("0"+number);
+        else
+          digit.html(number);
+      }
+    }
+	
+    $("#change").on('click', changeTimer);
+    $("#init").on('click', resetTimer);
+})
