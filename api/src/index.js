789390aba7cfcc159d4a2cca7be517274d061b64
Tiago <ottiagos@gmail.com>
2021-03-21 20:14:26 -0300

    Quick fix for server

--- api/src/index.js
+++ api/src/index.js
@@ -11,7 +11,7 @@ const acordeRouter = require('./routers/acordes');
 
 const app = express();
 
-const port = process.env.PORT ;
+const port = 3000;
 
 //Parses incoming request with json to objects
 app.use(cors());
