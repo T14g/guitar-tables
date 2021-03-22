ad03a6ca03c1df7a22bd3f75988c6b7440081d7b
Tiago <ottiagos@gmail.com>
2021-03-22 18:41:00 -0300

    Batch automation to start API,Mongo and Server

---
+++ run-app.bat
@@ -0,0 +1,5 @@
+@echo off
+START /min /d C:\js mongodb\bin\mongod.exe --dbpath=mongodb-data
+START /min cmd.exe /k "cd C:\js\guitar-tables\ && npm run server"
+START /min cmd.exe /k "cd C:\js\guitar-tables\api && npm start"
+START chrome http://127.0.0.1:8080/
